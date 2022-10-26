efda780423b6cc3e20415c47c9ddbe1d508855cf
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2022-10-26 20:55:53 +0200

    build: shrink setup.py to the necessary minimum
    Since project is now manged via Poetry and pyproject.toml, there is no need to
    duplicate things in setup.py

--- setup.py
+++ setup.py
@@ -10,36 +10,9 @@
 # ---------------------------------------------------------------------------
 import setuptools
 
-with open("README.md", "r") as fd:
-    long_description = fd.read()
+import pydio
 
 setuptools.setup(
-    name="PyDio",
-    author="Maciej Wiatrzyk",
-    author_email="maciej.wiatrzyk@gmail.com",
-    description="A simple and functional dependency injection toolkit for Python",
-    long_description=long_description,
-    url="https://pydio.readthedocs.io/",
-    packages=setuptools.find_packages(exclude=["docs", "tests*"]),
-    keywords="dependency, injection, di, framework, toolkit, tool, library",
-    python_requires=">=3.6, <4",
-    project_urls={
-        'Bug Reports': "https://gitlab.com/zef1r/pydio/-/issues",
-        'Source': "https://gitlab.com/zef1r/pydio",
-        'Documentation': "https://pydio.readthedocs.io/",
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        #"Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent",
-        #"Topic :: Software Development :: Libraries",
-    ],
+    name="pydio",
+    version=pydio.__version__
 )
