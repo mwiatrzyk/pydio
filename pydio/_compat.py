58dd96ba305ec40f49bb5c9a978d022bce03fb21
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2022-11-05 12:51:55 +0100

    feat: remove compatibility layer for no longer supported Python 3.6
    Since minimal supported Python version is 3.7.2, there is no point in
    keeping if-s for Py36, as it will not be even possible to install future
    versions on Py36.

--- pydio/_compat.py
+++ pydio/_compat.py
@@ -8,23 +8,6 @@
 #
 # See LICENSE.txt for details.
 # ---------------------------------------------------------------------------
-import sys
+from contextlib import AbstractAsyncContextManager
 
-_py_version = (sys.version_info.major, sys.version_info.minor)
-
-if _py_version >= (3, 7):
-
-    from contextlib import AbstractAsyncContextManager
-
-else:  # Py36
-
-    import abc
-
-    class AbstractAsyncContextManager(abc.ABC):
-
-        async def __aenter__(self):
-            return self
-
-        @abc.abstractmethod
-        async def __aexit__(self, exc_type, exc, tb):
-            pass
+__all__ = ['AbstractAsyncContextManager']
